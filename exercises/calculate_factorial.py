6c5872836be60b43892fd22c2d3dcaec93ccbc93
Ffollman <ffollman@student.touro.edu>
2025-02-19 23:17:08 -0500

    Updated main code and tests

--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,29 +1,45 @@
-# implement factorial_recursive method
+# Implement factorial_recursive method
 def factorial_recursive(number):
-    if number == 0 or number == 1:
+    validate_method_input(number)  # Ensure valid input before recursion
+    if number == 1 or number == 0:
         return 1
     return number * factorial_recursive(number - 1)
 
+def factorial_iterative(number):
+    validate_method_input(number)  # Ensure valid input before iteration
+    result = 1
+    while number > 1:
+        result *= number
+        number -= 1
+    return result
+
+def validate_method_input(number):
+    if not isinstance(number, int) or number < 0:
+        raise ValueError("Input must be a non-negative integer")
+
 def main():
     print("Factorial Computation Using Recursion")
+    string = input("Enter a non-negative integer: ")
 
+    number, message = process_input(string)
 
-    while True:
-        try:
-            number = int(input("Enter a non-negative integer: "))
+    if message:  # If there's an error message, display it and exit
+        print(message)
+        return
 
-            # handle negative input
-            if number < 0:
-                print("Error! To determine factorial, the number must be positive.")
-            else:
-                break
+    print_factorial(number)
 
-        except ValueError:
-            print("Error! Please enter a valid number")
+def print_factorial(number):
+    print(f"The factorial of {number} is {factorial_recursive(number)}")
 
-    # Call factorial_recursive method
-    result = factorial_recursive(number)
-    print(f"The factorial of {number} is {result}")
+def process_input(input_str):
+    try:
+        value = int(input_str)
+        if value < 0:
+            return None, 'Error: Number cannot be negative'
+        return value, ''
+    except ValueError:
+        return None, "Error: Input must be a non-negative integer"
 
 if __name__ == "__main__":
     main()
